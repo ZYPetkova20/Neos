2b67ee58eea103f6e686bf28e0bad0b5c765fe23
SABoev20 <85336777+SABoev20@users.noreply.github.com>
2023-03-19 01:02:00 +0200

    Add inputField and finish register
    Co-Authored-By: Zhivko Spasov <85338649+ZZSpasov20@users.noreply.github.com>

---
+++ src/client/neos.FrontendServices/include/inputField.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <raylib.h>
+#include <string>
+
+using std::string;
+
+namespace cp // namespace for our own-written compounds
+{
+    class inputField
+    {
+    private:
+
+        // Variables
+        Color textColor; // Color of the text
+        Rectangle field; // Position of the input field
+        string type; // Type of the input (text, email, password)
+        char input[30]; // The text from the field
+        float fontSize; // Size of the text
+        int charCount = 0; // Counts the inputed symbols
+        int maxChars; // Maximum characters in the field
+        unsigned animationCounter; // counts frames for the animation
+        bool isSelected = false;
+
+        // Fonts
+        Font font; // Font to use/display when typing
+
+        // Private Methods
+        void selectField(Vector2 mousePos); // checks if the field is selected
+        void printInput(); // Displays the text from input
+        void handleInput(); // Handles special symbols and other input
+        bool isKeyAllowed(); // Checks if the pressed key can be typed
+
+    public:
+        inputField(Rectangle field, string type, float fontSize, Color textColor);
+        ~inputField();
+
+        // Public Methods
+        void updateField(Vector2 mousePos); // Updates the field
+        string getResult(); // Returns the final result from the field
+    };
+}
